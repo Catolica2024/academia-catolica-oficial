e3934d7544285278c27f3930622be23d8f97e19f
Soporte Técnico <gpalma@colegiolacatolica.edu.pe>
2024-10-01 10:30:53 -0500

    cambio de diseño

--- js/slider.js
+++ js/slider.js
@@ -3,7 +3,27 @@ const swiper = new Swiper('#sliders', {
     autoplay: {
         delay: 7000,
         disableOnInteraction: false,
-      },
+    },
     effect: 'fade',
     speed: 1000
+});
+
+const swiper_docentes = new Swiper('#slider_docentes', {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1000,
+  breakpoints: {
+    650: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 2
+    },
+    1100: {
+      slidesPerView: 2
+    }
+  }
 });
